929e169a8b0863c6c267f7710523564686942d39
Mustafa Paksoy <paksoy@gmail.com>
2020-04-24 17:19:36 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
 
 function App() {
+  const [posts, setPosts] = useState([]);
+  async function getData() {
+    let resp = await axios.get("https://jsonplaceholder.typicode.com/posts");
+    setPosts(resp.data);
+  }
+
+  useEffect(function () {
+    getData();
+  }, []);
+
   return (
     <div className="App">
       <h1>Test</h1>
+      <ul>
+        {posts.map((post, i) => (
+          <li key={i}>{post.title}</li>
+        ))}
+      </ul>
     </div>
   );
 }
