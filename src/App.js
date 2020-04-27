9c2c12910f52b47974a328e0720228730eb27350
Mustafa Paksoy <paksoy@gmail.com>
2020-04-27 11:40:48 -0700

    Implement basic repl functionality

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component, useEffect, useState } from "react";
+import React, { Component } from "react";
 import axios from "axios";
 import "./App.css";
 
@@ -6,35 +6,67 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      posts: [{ title: "a" }],
+      blocks: [],
+      command: JSON.stringify({ hello: "world", count: -1 }),
     };
   }
 
-  componentDidMount() {
-    async function run(code) {
-      let resp = await axios.post("https://flatval.masfrost.repl.co/", {
-        code,
-        contextId: 1,
-      });
-      return resp.data;
-    }
+  async run(code) {
+    let resp = await axios.post("https://flatval.masfrost.repl.co/", {
+      code,
+    });
+    return resp.data.result;
+  }
 
-    async function getData() {
-      console.log(await run("var x = 1"));
-      console.log(await run("x"));
+  async submit(event) {
+    event.preventDefault();
+    const { command, blocks } = this.state;
+    let blockIndex = blocks.length;
+    this.setState({
+      blocks: blocks.concat({
+        command,
+      }),
+      command: "",
+    });
+
+    try {
+      let response = await this.run(command);
+      let block = this.state.blocks[blockIndex];
+      if (!block) {
+        // TODO handle error from race condition
+      }
+      this.setState({
+        blocks: this.state.blocks.map((block, index) =>
+          index === blockIndex ? { ...block, response } : block
+        ),
+      });
+    } catch (e) {
+      // TODO display error
     }
   }
 
   render() {
-    const { posts } = this.state;
     return (
       <div className="App">
-        <h1>Test2</h1>
-        <ul>
-          {posts.map((post, i) => (
-            <li key={i}>{post.title}</li>
+        <h1>Repl</h1>
+        <ul className="repl">
+          {this.state.blocks.map((block) => (
+            <li>
+              <div>{block.command}</div>
+              <pre>
+                {block.response && JSON.stringify(block.response, null, 2)}
+              </pre>
+            </li>
           ))}
         </ul>
+        <form onSubmit={this.submit.bind(this)}>
+          <input
+            tabIndex="0"
+            type="text"
+            value={this.state.command}
+            onChange={(e) => this.setState({ command: e.target.value })}
+          />
+        </form>
       </div>
     );
   }
