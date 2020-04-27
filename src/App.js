df914b3969961040b9e3ddbae9dab4d097dbfc30
Mustafa Paksoy <paksoy@gmail.com>
2020-04-27 11:14:44 -0700

    wip

--- src/App.js
+++ src/App.js
@@ -1,28 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React, { Component, useEffect, useState } from "react";
 import axios from "axios";
 import "./App.css";
 
-function App() {
-  const [posts, setPosts] = useState([]);
-  async function getData() {
-    let resp = await axios.get("https://jsonplaceholder.typicode.com/posts");
-    setPosts(resp.data);
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      posts: [{ title: "a" }],
+    };
   }
 
-  useEffect(function () {
-    getData();
-  }, []);
+  componentDidMount() {
+    async function run(code) {
+      let resp = await axios.post("https://flatval.masfrost.repl.co/", {
+        code,
+        contextId: 1,
+      });
+      return resp.data;
+    }
 
-  return (
-    <div className="App">
-      <h1>Test</h1>
-      <ul>
-        {posts.map((post, i) => (
-          <li key={i}>{post.title}</li>
-        ))}
-      </ul>
-    </div>
-  );
+    async function getData() {
+      console.log(await run("var x = 1"));
+      console.log(await run("x"));
+    }
+  }
+
+  render() {
+    const { posts } = this.state;
+    return (
+      <div className="App">
+        <h1>Test2</h1>
+        <ul>
+          {posts.map((post, i) => (
+            <li key={i}>{post.title}</li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
 }
 
 export default App;
